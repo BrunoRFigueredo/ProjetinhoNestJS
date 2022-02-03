00bb0f7a8734d7a93cb2c71663d472c96922462d
BETHA\bruno.figueredo <bruno.figueredo@betha.com.br>
2022-02-03 00:16:49 -0300

    Criando autenticação e autorização com JWT

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,15 +1,28 @@
-import { Controller, Post, Body } from '@nestjs/common';
+
+import {
+    Controller,
+    Post,
+    Body,
+    ValidationPipe,
+    UseGuards,
+} from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UsersService } from './users.service';
 import { ReturnUserDto } from './dto/return-user.dto';
+import { AuthGuard } from '@nestjs/passport';
+import { RolesGuard } from '../auth/roles.guard';
+import { Role } from '../auth/role.decorator';
+import { UserRole } from './user-roles.enum';
 
 @Controller('users')
 export class UsersController {
     constructor(private usersService: UsersService) {}
 
     @Post()
+    @Role(UserRole.ADMIN)
+    @UseGuards(AuthGuard(), RolesGuard)
     async createAdminUser(
-        @Body() createUserDto: CreateUserDto,
+        @Body(ValidationPipe) createUserDto: CreateUserDto,
     ): Promise<ReturnUserDto> {
         const user = await this.usersService.createAdminUser(createUserDto);
         return {
